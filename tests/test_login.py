46f581477463f4e956b7a72313f32366df43b6c1
meng-clb <mneg.20200606@gmail.com>
2025-05-30 00:02:09 +0800

    重命名文件名称

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,18 +1,19 @@
 import allure
 import pytest
-from pages.login_page import LoginPage
+from pages.login import LoginPage
 
 
-# 登录
-@allure.feature("登录功能")
-@allure.story("正常登录流程")
-@allure.title("登录成功")
-@pytest.mark.parametrize("username,password,office", [("chenlibin", "Start2025", "张江")])
-def test_login_access(dp, auto_screenshot, username, password, office):
-	login = LoginPage(dp, screenshot_step=auto_screenshot)
-	login.click_switch_page()
-	login.input_username(username)
-	login.input_password(password)
-	login.click_login()
-	login.select_office(office)
-	assert '输入病历号' in login.get_search_text()
+class TestLogin:
+	# 登录
+	@allure.feature("登录功能")
+	@allure.story("正常登录流程")
+	@allure.title("登录成功")
+	@pytest.mark.parametrize("username,password,office", [("chenlibin", "Start2025", "张江")])
+	def test_login_access(dp, auto_screenshot, username, password, office):
+		login = LoginPage(dp, screenshot_step=auto_screenshot)
+		login.click_switch_page()
+		login.input_username(username)
+		login.input_password(password)
+		login.click_login()
+		login.select_office(office)
+		assert '输入病历号' in login.get_search_text()
